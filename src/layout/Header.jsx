9269ca2ce00114aa9b482e9b1336f5d19860574c
Sergii Tsekhmister <tsehmistersergey@gmail.com>
2022-05-16 12:12:09 +0300

    fixed links

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -4,7 +4,7 @@ export function Header() {
       <div className="nav-wrapper">
         <a href="!#" className="brand-logo">React Movies</a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li><a href="collapsible.html">Repository</a></li>
+          <li><a href="https://github.com/Tsekhmister/react_movies">Repository</a></li>
         </ul>
       </div>
     </nav>
