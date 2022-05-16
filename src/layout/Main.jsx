05fd16d12305a1d431c278764d2b8cb6d17984a4
Sergii Tsekhmister <tsehmistersergey@gmail.com>
2022-05-16 12:05:14 +0300

    fixes url adress

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -12,21 +12,31 @@ export class Main extends React.Component {
   };
 
   componentDidMount() {
-    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=citizen`)
+    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=citizen`)
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search, loading: false }));
+      .then((data) => this.setState({ movies: data.Search, loading: false })
+      )
+      .catch((error) => {
+        console.error(error);
+        this.setState({ loading: false });
+      });
   }
 
   searchMovies = (str, type = 'all') => {
     this.setState({ loading: true });
 
     fetch(
-      `http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
         type !== 'all' ? `&type=${type}` : ''
       }`
     )
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search, loading: false }));
+      .then((data) => this.setState({ movies: data.Search, loading: false })
+      )
+      .catch((error) => {
+        console.error(error);
+        this.setState({ loading: false });
+      });
   };
 
   render() {
